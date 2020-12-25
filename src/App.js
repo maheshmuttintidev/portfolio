a1f0a9cad4ed4c0ca2e47e5822a52d6700951726
Mahesh Muttinti <71684468+mahesh-muttinti@users.noreply.github.com>
2020-12-25 22:32:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
+import ReactDom from "react-dom";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Mahesh Muttinti</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNSrKYplxMr-PC7toluw3E0PA3icvqPId9AQ&usqp=CAU" />
+      <h2>This is a home page</h2>
     </div>
   );
 }
