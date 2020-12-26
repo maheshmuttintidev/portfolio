5aa2f622e0592b83892b4c675ed4e4e051ec5aeb
mahesh-muttinti <maheshmuttinti@gmail.com>
2020-12-26 22:48:32 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Mahesh Muttinti</h1>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNSrKYplxMr-PC7toluw3E0PA3icvqPId9AQ&usqp=CAU" />
-      <h2>This is a home page</h2>
+    <div className="app-page">
+      <header>
+        <h1 className="main-heading">Portfolio</h1>
+      </header>
+      <article>
+        <div className="profile-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNSrKYplxMr-PC7toluw3E0PA3icvqPId9AQ&usqp=CAU"
+            className="avatar"
+          />
+          <div className="introduction">
+            <table>
+              <tr>
+                <th>Name: </th>
+                <td>Mahesh Muttinti</td>
+              </tr>
+              <tr>
+                <th>Skills: </th>
+                <td>HTML, CSS, JS</td>
+              </tr>
+              <tr>
+                <th>Hobbies: </th>
+                <td>Playing Football, Coding and Listening to Music</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </article>
     </div>
   );
 }
