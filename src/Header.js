cbb496b7dd0adbfbc033f389e63a3f61248bef0d
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-09 22:51:40 +0530

    2 files modified, 3 added

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo-container" />
+      <div className="website-name-container">
+        <h2 className="website-name">WEB THOUGHTS</h2>
+      </div>
+      <h1 className="main-heading">
+        This Website gives you the simple HTML, CSS and JS Tutorials with
+        Examples
+      </h1>
+    </header>
+  );
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,21 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Article from "./Article";
 import { ProgressBar } from "react-bootstrap";
+import Footer from "./Footer";
 
 export default function HomePage() {
-  const [html, css, js, java, python, react, node] = [75, 65, 35, 25, 10, 5];
+  
   return (
     <div className="app-page">
-      <header>
-        <div className="logo-container" />
-        <div className="website-name-container">
-          <h2 className="website-name">WEB THOUGHTS</h2>
-        </div>
-        <h1 className="main-heading">
-          This Website gives you the simple HTML, CSS and JS Tutorials with
-          Examples
-        </h1>
-      </header>
-      <article>
+    <Header />
+    <article>
+      <div className="profile-card-container">
+        <h1 className="author-intro-heading">About Author:</h1>
         <div className="profile-card">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNSrKYplxMr-PC7toluw3E0PA3icvqPId9AQ&usqp=CAU"
@@ -40,36 +36,32 @@ export default function HomePage() {
             codes in different platforms like sololearn, codepen etc.,
           </div>
         </div>
-        <div className="skills-container">
-          <h1 className="skill-heading">Skills:</h1>
-          <div className="progressBar">
-            <ProgressBar now={html} label={`HTML`} /><br />
-            <ProgressBar now={css} label={`CSS`} /><br />
-            <ProgressBar now={js} label={`JS`} /><br />
-            <ProgressBar now={java} label={`JAVA`} /><br />
-            <ProgressBar now={java} label={`PYTHON`} /><br />
-            <ProgressBar now={java} label={`REACT`} /><br />
-            <ProgressBar now={java} label={`NODE`} /><br />
-          </div>
+      </div>
+      <div className="skills-container">
+        <h1 className="skill-heading">Skills:</h1>
+        <div className="progressBar">
+          <ProgressBar now={html} label={`HTML`} style={{ width: "300px" }} />
+          <br />
+          <ProgressBar now={css} label={`CSS`} style={{ width: "300px" }} />
+          <br />
+          <ProgressBar now={js} label={`JS`} style={{ width: "300px" }} />
+          <br />
+          <ProgressBar now={java} label={`JAVA`} style={{ width: "300px" }} />
+          <br />
+          <ProgressBar
+            now={python}
+            label={`PYTHON`}
+            style={{ width: "300px" }}
+          />
+          <br />
+          <ProgressBar now={react} label={`REACT`} style={{ width: "300px" }} />
+          <br />
+          <ProgressBar now={node} label={`NODE`} style={{ width: "300px" }} />
+          <br />
         </div>
-      </article>
-      <footer>
-        <div className="follow-container">
-          <h2 className="sub-heading follow">Follow me on: </h2>
-          <div className="holder btn-holder">
-            <button className="btn btn-green">facebook</button>
-            <button className="btn btn-green">instagram</button>
-            <button className="btn btn-green">github</button>
-            <button className="btn btn-green">codepen</button>
-            <button className="btn btn-green">sololearn</button>
-          </div>
-        </div>
-        <div className="copyright-container">
-          <p>
-            <a href="#">Mahesh Muttinti Creatives</a> &copy; 2021
-          </p>
-        </div>
-      </footer>
+      </div>
+    </article>
+      
     </div>
   );
 }
