4144a7461db459153cbd4ae8d175a8d45837b01d
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-09 19:03:27 +0530

    2 files modified

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,14 @@ export default function HomePage() {
   return (
     <div className="app-page">
       <header>
-        <h1 className="main-heading">Portfolio</h1>
+        <div className="logo-container" />
+        <div className="website-name-container">
+          <h2 className="website-name">WEB THOUGHTS</h2>
+        </div>
+        <h1 className="main-heading">
+          This Website gives you the simple HTML, CSS and JS Tutorials with
+          Examples
+        </h1>
       </header>
       <article>
         <div className="profile-card">
@@ -13,34 +20,40 @@ export default function HomePage() {
             className="avatar"
           />
           <div className="introduction">
-            <p>
-              <em style={{ fontSize: "1.5em" }}>Hi, there!</em> <br />{" "}
-              &nbsp;&nbsp;&nbsp;My name is <strong>Mahesh Muttinti</strong>. I
-              am a web designer and also developer. I am learning full stack
-              development in which my stack to develop the web apps is{" "}
-              <strong>MERN.</strong>
-              <br />
-              <ul>
-                <li>MongoDB refers to M</li>
-                <li>Express JS refers to E</li>
-                <li>React JS refers to R</li>
-                <li>Node JS refers to N</li>
-              </ul>
-              <br />
-              My Hobbies include playing football, listening to music and post
-              codes in different platforms like sololearn, codepen etc.,{" "}
-            </p>
+            <em style={{ fontSize: "1.5em" }}>Hi, there!</em> <br />
+            &nbsp;&nbsp;&nbsp;My name is <strong>Mahesh Muttinti</strong>. I am
+            a web designer and also developer. I am learning full stack
+            development in which my stack to develop the web apps is
+            <strong> MERN.</strong>
+            <br />
+            <br />
+            <ul>
+              <li>MongoDB refers to M</li>
+              <li>Express JS refers to E</li>
+              <li>React JS refers to R</li>
+              <li>Node JS refers to N</li>
+            </ul>
+            <br />
+            My Hobbies include playing football, listening to music and post
+            codes in different platforms like sololearn, codepen etc.,
           </div>
         </div>
       </article>
       <footer>
-        <h2 className="sub-heading follow">Follow me on: </h2>
-        <div className="holder btn-holder">
-          <button className="btn btn-green">facebook</button>
-          <button className="btn btn-green">instagram</button>
-          <button className="btn btn-green">github</button>
-          <button className="btn btn-green">codepen</button>
-          <button className="btn btn-green">sololearn</button>
+        <div className="follow-container">
+          <h2 className="sub-heading follow">Follow me on: </h2>
+          <div className="holder btn-holder">
+            <button className="btn btn-green">facebook</button>
+            <button className="btn btn-green">instagram</button>
+            <button className="btn btn-green">github</button>
+            <button className="btn btn-green">codepen</button>
+            <button className="btn btn-green">sololearn</button>
+          </div>
+        </div>
+        <div className="copyright-container">
+          <p>
+            <a href="#">Mahesh Muttinti Creatives</a> &copy; 2021
+          </p>
         </div>
       </footer>
     </div>
