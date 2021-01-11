56edcab4433a182434cbc75b9ccad3b863cc5e6b
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-11 11:00:37 +0530

    2 files modified

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Article from "./Article";
 import Footer from "./Footer";
+import "bootstrap";
 export default function HomePage() {
   return (
     <div className="app-page">
