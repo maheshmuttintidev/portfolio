3ed21e021da712fe83d3f9ca0ef58864ce4f104a
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-17 18:14:29 +0530

    install logrocket to app

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,10 @@ import Hero from "./Hero";
 import Article from "./Article";
 import Footer from "./Footer";
 import "bootstrap";
+import LogRocket from 'logrocket';
+
+LogRocket.init('3hchfb/mahesh-muttinti-portfolio');
+
 function HomePage() {
   return (
     <div className="app-page">
