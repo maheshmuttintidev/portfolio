96b0e3b2a57a546b7b6fac9e1d12539e975e0f00
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-17 23:48:09 +0530

    simplify the portfolio

--- src/Footer.js
+++ src/Footer.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./style.css";
+import 'font-awesome/css/font-awesome.min.css';
 
 function Footer() {
   return (
     <footer>
-      <div className="follow-container">
-        <h2 className="sub-heading follow">Follow me on: </h2>
-        <div className="holder btn-holder">
-          <button className="btn btn-green">facebook</button>
-          <button className="btn btn-green">instagram</button>
-          <button className="btn btn-green">github</button>
-          <button className="btn btn-green">codepen</button>
-          <button className="btn btn-green">sololearn</button>
-        </div>
-      </div>
-      <div className="copyright-container">
-        <p>
-          <a href="#">Mahesh Muttinti Creatives</a> &copy; 2021
-        </p>
+      <h2 className="follow-heading">Follow Me On:</h2>
+      <div className="social-media-icon-container">
+        <i className="fab fa-facebook"></i>
+        <i className="fab fa-twitter"></i>
+        <i className="fab fa-github"></i>
+        <i className="fab fa-stack-overflow"></i>
+        <i className="fab fa-linkedin"></i>
       </div>
     </footer>
   );
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,16 @@
 import React from "react";
 import Hero from "./Hero";
-import Article from "./Article";
 import Footer from "./Footer";
-import "bootstrap";
 import LogRocket from 'logrocket';
 
 LogRocket.init('3hchfb/mahesh-muttinti-portfolio');
 
 function HomePage() {
   return (
-    <div className="app-page">
+    <>
       <Hero />
-      <Article />
       <Footer />
-    </div>
+    </>
   );
 }
 export default HomePage;
