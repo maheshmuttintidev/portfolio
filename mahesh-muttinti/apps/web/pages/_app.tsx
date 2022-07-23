822206af0358c419ee2843da34511b2dfee7b52f
maheshmuttintidev <maheshmuttintidev@gmail.com>
2022-07-23 20:38:25 +0530

    fix: type errors

--- mahesh-muttinti/apps/web/pages/_app.tsx
+++ mahesh-muttinti/apps/web/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../index.css'
 import * as React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Mahesh Muttinti Portfolio | Mahesh Muttinti</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
