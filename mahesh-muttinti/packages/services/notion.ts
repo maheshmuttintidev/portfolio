e1bf03efe16a854df63349bd634b46da60fdfa38
maheshmuttinti <maheshmuttinti@betalectic.com>
2022-11-26 23:17:47 +0530

    show the about me description from notion page

--- mahesh-muttinti/packages/services/notion.ts
+++ mahesh-muttinti/packages/services/notion.ts
@@ -1,65 +1,13 @@
-import { axiosObject } from "./setup";
+import { Client } from "@notionhq/client";
+const client = new Client({
+  auth: process.env.NOTION_SECRET_KEY,
+});
 
-// function createPage(payload: any) {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       let endPoint = "/page";
-//       let apiPayload = { ...payload };
-//       let response = await axiosObject.post(endPoint, apiPayload);
-//       console.log("response", response);
-//       if (response) {
-//         resolve(response);
-//       }
-//       resolve(null);
-//     } catch (err) {
-//       reject(err);
-//     }
-//   });
-// }
-
-// function getPosts() {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       let endPoint = "/pages";
-//       let response = await axiosObject.get(endPoint);
-//       console.log("response", response);
-//       if (response) {
-//         resolve(response);
-//       }
-//       resolve(null);
-//     } catch (err) {
-//       reject(err);
-//     }
-//   });
-// }
-
-var config = {
-  method: "get",
-  url: "https://api.notion.com/v1/pages/df991b51-62c2-4a85-81a4-98749f5ec877",
-  headers: {
-    "Notion-Version": "2022-02-22",
-    Authorization: "Bearer secret_5ytLqUHUO5argNM2pAo8sPA7iq9QmNS5WsmsJOD6Xbw",
-  },
-};
-
-function getPortfolio() {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let endPoint = "/pages/df991b51-62c2-4a85-81a4-98749f5ec877";
-      let response = await axiosObject.get(endPoint);
-      console.log("response", response);
-      if (response) {
-        resolve(response.data);
-      }
-      resolve(null);
-    } catch (err) {
-      reject(err);
-    }
+async function getMyPortfolioContent() {
+  const myPosts = await client.blocks.children.list({
+    block_id: "df991b5162c24a8581a498749f5ec877",
   });
+  return myPosts;
 }
 
-export {
-  //  createPage,
-  // getPosts,
-  getPortfolio,
-};
+export { getMyPortfolioContent };
