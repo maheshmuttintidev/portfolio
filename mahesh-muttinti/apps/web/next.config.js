8871566bfdc72a80e142605a6fb87106ebea4b87
maheshmuttinti <maheshmuttinti@betalectic.com>
2023-05-20 13:00:03 +0530

    initialize xata into `nextjs` portfolio

--- mahesh-muttinti/apps/web/next.config.js
+++ mahesh-muttinti/apps/web/next.config.js
@@ -4,9 +4,13 @@ module.exports = withTM({
   reactStrictMode: true,
   env: {
     NOTION_SECRET_KEY: process.env.NOTION_SECRET_KEY,
+    XATA_DATABASE_URL: process.env.XATA_DATABASE_URL,
+    XATA_BRANCH: process.env.XATA_BRANCH,
+    XATA_API_KEY: process.env.XATA_API_KEY,
   },
   experimental: {
     runtime: 'edge',
+    enableBrowser: true,
   },
   rewrites: async () => {
     return [
