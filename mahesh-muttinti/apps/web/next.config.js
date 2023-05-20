600ea40b4496b90fbe64595d5632fda8d37b16c6
maheshmuttinti <maheshmuttinti@betalectic.com>
2023-05-20 13:08:49 +0530

    chore: resolve conflicts

--- mahesh-muttinti/apps/web/next.config.js
+++ mahesh-muttinti/apps/web/next.config.js
@@ -9,12 +9,9 @@ module.exports = withTM({
     XATA_API_KEY: process.env.XATA_API_KEY,
   },
   experimental: {
-<<<<<<< HEAD
     runtime: 'edge',
     enableBrowser: true,
-=======
     runtime: 'experimental-edge',
->>>>>>> 102f8a4589af4cd9c665ff0ebb91606d2e81669b
   },
   rewrites: async () => {
     return [
