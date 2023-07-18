7d46b64fccd7eb6442600b7b618ffb5665db2f12
maheshmuttintidev <maheshmuttintidev@gmail.com>
2023-07-18 14:49:22 +0530

    suppress ts error

--- mahesh-muttinti/apps/web/src/app/blogs/[slug]/page.tsx
+++ mahesh-muttinti/apps/web/src/app/blogs/[slug]/page.tsx
@@ -3,16 +3,22 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getSinglePost } from "services";
 
+interface SingleBlogTypes {
+  id?: string;
+}
+
 export default async function Page() {
   const params = useParams();
+  
 
   const slug = params?.slug?.split("-").join(" ");
   // const singleBlog = await getSinglePost(`${slug}`);
-  const [singleBlog, setSingleBlog] = useState<null>(null);
+  const [singleBlog, setSingleBlog] = useState<SingleBlogTypes | null>(null);
   useEffect(() => {
     (async () => {
       try {
         const data = await getSinglePost(`${slug}`);
+        // @ts-ignore
         setSingleBlog(data);
       } catch (error) {
         console.log("errorr", error);
