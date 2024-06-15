56434d623bd25b8b102777dde995e5060bad91a6
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-06-15 22:24:39 +0530

    ui fixes

--- apps/web/src/components/about-me-description.tsx
+++ apps/web/src/components/about-me-description.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import { skills } from "./skills-list";
 import Skill from "./skill-card";
 
@@ -20,36 +20,36 @@ export const AboutMeDescription = () => {
       className="container flex flex-wrap gap-4 justify-end items-center p-3"
     >
       <div className="flex flex-wrap justify-center bg-[url('https://i.ibb.co/drYZ6xh/dev-pose-in-chair.png')] h-screen bg-contain bg-no-repeat">
-        <div className="flex flex-wrap gap-5">
-         <div className="flex-1/2">
-         <p className="md:text-3xl text-4xl text-white text-center font-['Inter'] font-bold">
-            More About Me:
-          </p>
+        <div className="flex flex-wrap gap-5 justify-center items-center">
+          <div className="">
+            <p className="md:text-3xl text-4xl text-white text-center font-['Inter'] font-bold">
+              More About Me:
+            </p>
 
-          <p className="mt-3 text-center font-['Inter'] md:text-2xl text-xl">
-            {`I'm Mahesh Muttinti, a Full Stack Web & Mobile Application Developer
+            <p className="mt-3 text-center font-['Inter'] md:text-2xl text-xl">
+              {`I'm Mahesh Muttinti, a Full Stack Web & Mobile Application Developer
             with a passion for creating awesome digital experiences. With 3
             years of experience in React JS, React Native, and Node JS, I
             specialize in building stunning websites and mobile apps`}
-          </p>
-         </div>
-
-          <div className="flex-1/2">
-          <h3 className="md:text-3xl text-4xl font-bold font-['Inter'] py-4">
-            Technical Skills:
-          </h3>
-          <div className="flex gap-3 flex-wrap justify-center items-center">
-            {skills?.map((skill, index) => (
-              <Skill
-                key={`skill_${index}`}
-                title={skill?.title || ""}
-                text={skill?.text || ""}
-                imgUrl={skill?.imgUrl || ""}
-                percentage={skill?.percentage || 0}
-                length={skills?.length}
-              />
-            ))}
+            </p>
           </div>
+
+          <div className="">
+            <h3 className="md:text-3xl text-4xl font-bold font-['Inter'] py-4">
+              Technical Skills:
+            </h3>
+            <div className="flex gap-3 flex-wrap justify-center items-center">
+              {skills?.map((skill, index) => (
+                <Skill
+                  key={`skill_${index}`}
+                  title={skill?.title || ""}
+                  text={skill?.text || ""}
+                  imgUrl={skill?.imgUrl || ""}
+                  percentage={skill?.percentage || 0}
+                  length={skills?.length}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- apps/web/src/components/hero-section-1.tsx
+++ apps/web/src/components/hero-section-1.tsx
@@ -8,16 +8,16 @@ export const HeroSectionOne = () => {
   return (
     <div>
       <section className="flex flex-col gap-3 flex-wrap items-center justify-center dark:bg-[#0E1339] h-screen w-full bg-[url('https://i.ibb.co/hKH07Sg/hero.png')] bg-contain bg-left bg-no-repeat">
-        <div className="flex flex-col gap-5 justify-center items-center self-end text-center">
+        <div className="flex flex-col gap-5 justify-center items-center md:self-end self-center text-center px-3">
           <h1 className="text-blue-500 leading-[2.5rem] text-center pt-4">
-            <span className="text-8xl text-white font-['Jomhuria']">
+            <span className="md:text-8xl text-6xl text-white font-['Jomhuria']">
               This is{" "}
             </span>
-            <span className="text-[5rem] text-white font-['Holtwood One SC']">
+            <span className="md:text-[5rem] text-[3rem] text-white font-['Holtwood One SC']">
               Mahesh Muttinti
             </span>
             <br />
-            <span className="text-8xl text-yellow-500 underline font-['Jomhuria']">
+            <span className="md:text-8xl text-6xl text-yellow-500 underline font-['Jomhuria']">
               Full Stack Web & Mobile Developer
             </span>{" "}
           </h1>
