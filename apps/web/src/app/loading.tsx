46855c65f4a7433f7cbe42e3f211fdeaec502954
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-06-16 16:03:52 +0530

    fix loading component long spaces

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -1,12 +1,12 @@
 export default function Loading() {
   return (
-    <div className="container mx-auto flex flex-col px3">
+    <div className="container mx-auto flex flex-col px-3 mt-20 md:mt-3">
       {Array(20)
         .fill(1)
         .map((_, index) => {
           return (
             <div
-              className="skeleton-loading mt-20 md:mt-3 h-10 my-2 w-full bg-gradient-to-r from-slate-100 to-slate-500 rounded-md"
+              className="skeleton-loading h-10 my-2 w-full bg-gradient-to-r from-slate-100 to-slate-500 rounded-md"
               key={`skeleton_${index}`}
             ></div>
           );
