ef26dbb4161e12ec114337acf46085a7723353ac
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-06-22 18:51:20 +0530

    update sh scripts

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "First arg: $1"
 echo "Second arg: $2"
-cp="cp apps/maheshMuttintiRnMobileApp/android/app/build/outputs/bundle/release/app-release.aab ~/Desktop/`date +%d_%m_%Y_%H_%M`_maheshMuttintiRnMobileApp_$1_$2.aab"
+cp="cp apps/mobile/android/app/build/outputs/bundle/release/app-release.aab ~/Desktop/`date +%d_%m_%Y_%H_%M`_maheshmuttinti_$1_$2.aab"
 
 eval "$cp"
--- copy_apk.sh
+++ copy_apk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "First arg: $1"
 echo "Second arg: $2"
-cp="cp apps/maheshMuttintiRnMobileApp/android/app/build/outputs/apk/release/app-release.apk ~/Desktop/`date +%d_%m_%Y_%H_%M`_maheshMuttintiRnMobileApp_$1_$2.apk"
+cp="cp apps/mahesh/android/app/build/outputs/apk/release/app-release.apk ~/Desktop/`date +%d_%m_%Y_%H_%M`_maheshmuttinti_$1_$2.apk"
 
 eval "$cp"
