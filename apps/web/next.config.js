4b2ebc44d9e7e870921b2cc0d063f92899ca4057
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-06-28 16:31:32 +0530

    test pathname

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,22 +8,22 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "i.ibb.co",
-        pathname: "maheshmuttintidev.in/*",
+        pathname: "maheshmuttintidev/*",
       },
       {
         protocol: "https",
         hostname: "s3.us-west-2.amazonaws.com",
-        pathname: "maheshmuttintidev.in/*",
+        pathname: "maheshmuttintidev/*",
       },
       {
         protocol: "https",
         hostname: "via.placeholder.com",
-        pathname: "maheshmuttintidev.in/*",
+        pathname: "maheshmuttintidev/*",
       },
       {
         protocol: "https",
         hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
-        pathname: "maheshmuttintidev.in/*",
+        pathname: "maheshmuttintidev/*",
       },
     ],
   },
